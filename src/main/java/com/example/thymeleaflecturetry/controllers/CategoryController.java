e6c63f5a8207cbce96c2ea372b4cac1e8f00aa6e
roudabehadnani <rodabeh.adnani@gmail.com>
2022-10-16 17:04:01 +0200

    Adding category

--- src/main/java/com/example/thymeleaflecturetry/controllers/CategoryController.java
+++ src/main/java/com/example/thymeleaflecturetry/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.example.thymeleaflecturetry.controllers;
 
+import com.example.thymeleaflecturetry.model.dto.form.CategoryForm;
 import com.example.thymeleaflecturetry.model.dto.view.CategoryView;
 import org.apache.catalina.LifecycleState;
 import org.springframework.stereotype.Controller;
@@ -29,9 +30,12 @@ public class CategoryController {
 
         LocalDate currentDate = LocalDate.now();
         model.addAttribute("serverDate", currentDate);
-        model.addAttribute("categoryListSize", categoryViewList.size());
+//        model.addAttribute("categoryListSize", categoryViewList.size()); we don not need anymore
 
         model.addAttribute("categoryViews",categoryViewList);
+
+        CategoryForm categoryForm = new CategoryForm();
+        model.addAttribute("category",categoryForm);
 
         return "category/categories-view";
     }
@@ -58,6 +62,28 @@ public class CategoryController {
         model.addAttribute("categoryView", categoryView );
 
         return "category/category-view";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable("id") Integer id){
+
+        categoryViewList.removeIf(view -> view.getId() == id);
+
+       return "redirect:/category/list";   //redirect to method with mapping /list
+//       return "category/categories-view"; HTML File
+    }
+
+    @PostMapping("/add")
+    public String addCategory(@ModelAttribute("category") CategoryForm categoryForm ){
+
+        System.out.println("categoryForm" + categoryForm);
+
+        int randNum = (int) (Math.random()  *100);
+        CategoryView categoryToAdd = new CategoryView(randNum, categoryForm.getName(),LocalDate.now());
+        categoryViewList.add(categoryToAdd);
+
+        return "redirect:/category/list";
+
     }
 
 
--- src/main/java/com/example/thymeleaflecturetry/model/dto/form/CategoryForm.java
+++ src/main/java/com/example/thymeleaflecturetry/model/dto/form/CategoryForm.java
@@ -1,4 +1,13 @@
 package com.example.thymeleaflecturetry.model.dto.form;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CategoryForm {
+
+    private String name;
 }
